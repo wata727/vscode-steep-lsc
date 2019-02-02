b01a33e7856eb116ad886f829e59e2a26c6b4167
wata_mac <watassbass@gmail.com>
2019-02-03 02:20:08 +0900

    It works

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,21 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const serverOptions: ServerOptions = {
+		command: "bundle",
+		args: ["exec", "steep", "langserver"],
+	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "steep-lsc" is now active!');
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: ['ruby'],
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	const disposable = new LanguageClient("Steep", serverOptions, clientOptions).start()
 
 	context.subscriptions.push(disposable);
 }
